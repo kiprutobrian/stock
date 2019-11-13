357d036034a6cdd0b5a142511c1be73408b4a194
kiprutobrian <briankipruto793@gmail.com>
2019-11-13 19:49:29 +0300

    added portfolio module to vuex

---
+++ src/store/modules/portfolio.js
@@ -0,0 +1,66 @@
+const state = {
+    funds: 10000,
+    stocks: []
+};
+const mutations = {
+    'BUY_STOCK'(state, {
+        stockId,
+        quantity,
+        stockPrice
+    }) {
+        const record = state.stocks.find(element => element.id == stockId);
+        if (record) {
+            record.quantity += quantity;
+        } else {
+            state.stocks.push({
+                id: stockId,
+                quantity: quantity
+            });
+        }
+        state.stocks -= stocksPrice * quantity;
+    },
+    'SELL_STOCK'(state, {
+        stockId,
+        quantity,
+        stockPrice
+    }) {
+        const record = state.stocks.find(element => element.id == stockId);
+        if (record.quantity > quantity) {
+            record.quantity -= quantity;
+        } else {
+            state.stocks.splice(state.stocks.indexOf(record), 1);
+        }
+        state.funds += stocksPrice * quantity;
+    },
+};
+const actions = {
+    sellStock: ({
+            commit
+        },
+        order
+    ) => {
+        commit('SELL_STOCK', order);
+    },
+};
+const getters = {
+    stockPortfolio: (state, getters) => {
+        return state.stocks.map(stock => {
+            const recor = getters.stocks.find(element => element.id == stock.id);
+            return {
+                id: stock.id,
+                quantity: stock.quantity,
+                name: record.name,
+                price: record.price
+            }
+        });
+    },
+    funds(state) {
+        return state.funds;
+    }
+};
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
