475121db65e9c12496d01cbba12afbb6d7d2607b
kiprutobrian <briankipruto793@gmail.com>
2019-11-13 16:00:14 +0300

    Adding a buy button

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="row">
-		<app-stock v-for="(stock, index) in stocks"></app-stock>
+		<app-stock v-for="stock in stocks" :key="stock" :stock="stock"></app-stock>
 	</div>
 </template>
 
